0afaec4b302a5927c4514887caf570923595b4a0
Ghazi18 <suruji.188@gmail.com>
2024-02-08 17:13:00 +0300

    add animation to the pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,51 +1,59 @@
 import { Container } from "react-bootstrap";
-import searchIcon from '../images/search 1.svg';
-import Footer from '../components/Footer';
+import searchIcon from "../images/search 1.svg";
+import Footer from "../components/Footer";
 import UserReviewPage from "../components/UserReviewPage";
-import FamousPlant from '../components/FamousPlant';
-import Prodcuts from '../components/Products';
+import FamousPlant from "../components/FamousPlant";
+import Prodcuts from "../components/Products";
 import Header from "../components/Header";
+import AnimatedPage from "../components/AnimatedPage";
 
 function Home() {
-    return (
-        <>
-            <div className="main-hero border-bottom-rad pb-3">
-                <Header />
-                <Container className="text-center position-relative">
-                    <div className="search-div">
-                        <input type="search" placeholder="البحث" />
-                        <img className="search-icon" src={searchIcon} alt="search-icon" />
-                    </div>
-                    <div className="text-hero mt-5">
-                        <div className="title my-3">
-                            <h1>مرحباً بك في </h1>
-                            <span>نبتتي</span>
-                        </div>
-                        <p> المكان المثالي لاكتشاف عالم النباتات <span>وجمالها!</span></p>
-                    </div>
-                </Container>
+  return (
+    <>
+      <AnimatedPage>
+        <div className="main-hero border-bottom-rad pb-3">
+          <Header />
+          <Container className="text-center position-relative">
+            <div className="search-div">
+              <input type="search" placeholder="البحث" />
+              <img className="search-icon" src={searchIcon} alt="search-icon" />
             </div>
-            <div className="famous-plants">
-                <Container className=" position-relative">
-                    <div className="caption text-center pt-3 mt-5">
-                        <h2 className="text-title fw-bold">تَعرّف على النباتات وميزاتها من خلال القصص الممتعة! </h2>
-                    </div>
-                </Container>
-                <FamousPlant />
+            <div className="text-hero mt-5">
+              <div className="title my-3">
+                <h1>مرحباً بك في </h1>
+                <span>نبتتي</span>
+              </div>
+              <p>
+                {" "}
+                المكان المثالي لاكتشاف عالم النباتات <span>وجمالها!</span>
+              </p>
             </div>
-            <div className="caption text-center py-4">
-                <h2 className="text-title fw-bold">منتجاتنا</h2>
-                <p className="descrp">نقدم في نبتتي أفضل النباتات الداخلية</p>
+          </Container>
+        </div>
+        <div className="famous-plants">
+          <Container className=" position-relative">
+            <div className="caption text-center pt-3 mt-5">
+              <h2 className="text-title fw-bold">
+                تَعرّف على النباتات وميزاتها من خلال القصص الممتعة!{" "}
+              </h2>
             </div>
-            <Container>
-                <Prodcuts />
-            </Container>
-            <Container className="review-container py-4">
-                <h2 className="text-title fw-bold" >تجارب عملاءَنا</h2>
-                <UserReviewPage />
-            </Container>
-            <Footer />
-        </>
-    );
+          </Container>
+          <FamousPlant />
+        </div>
+        <div className="caption text-center py-4">
+          <h2 className="text-title fw-bold">منتجاتنا</h2>
+          <p className="descrp">نقدم في نبتتي أفضل النباتات الداخلية</p>
+        </div>
+        <Container>
+          <Prodcuts />
+        </Container>
+        <Container className="review-container py-4">
+          <h2 className="text-title fw-bold">تجارب عملاءَنا</h2>
+          <UserReviewPage />
+        </Container>
+        <Footer />
+      </AnimatedPage>
+    </>
+  );
 }
 export default Home;
--- src/Pages/OurProducts.js
+++ src/Pages/OurProducts.js
@@ -2,19 +2,22 @@ import { Container } from "react-bootstrap";
 import Prodcuts from "../components/Products";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import AnimatedPage from "../components/AnimatedPage";
 function OurProducts() {
-    return (
-        <>
+  return (
+    <>
+      <AnimatedPage>
         <Header />
-            <div className="caption text-center py-4">
-            <h2 className="fw-bold">منتجاتنا</h2>
-                <p className="descrp">نقدم في نبتتي أفضل النباتات الداخلية</p>
-            </div>
-            <Container className="py-4">
-                <Prodcuts />
-            </Container>
-            <Footer />
-        </>
-    )
+        <div className="caption text-center py-4">
+          <h2 className="fw-bold">منتجاتنا</h2>
+          <p className="descrp">نقدم في نبتتي أفضل النباتات الداخلية</p>
+        </div>
+        <Container className="py-4">
+          <Prodcuts />
+        </Container>
+        <Footer />
+      </AnimatedPage>
+    </>
+  );
 }
 export default OurProducts;
