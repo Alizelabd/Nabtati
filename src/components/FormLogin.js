2f5c54af45a0e2a5c46a100a4c0ec0c9881f1adc
Alizelabd <lordlords834@gmail.com>
2024-02-10 00:56:18 +0300

    edite from alezi

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -2,51 +2,52 @@ import React from "react";
 import { useState } from "react";
 import ProductInquiryForm from "./ProductInquiryForm";
 
-const FormLogin=()=>{
-    const [show, setShow] = useState(false);
-    const [showAlert, setShowAlert] = useState(false);
-    const [setStateMessage] = useState('');
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-   return(
-    
+const FormLogin = () => {
+  const [show, setShow] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [setStateMessage] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+
     <>
-     <button type="button" class="btn btn-primary" className='sign-link sign-in' data-bs-toggle="modal" data-bs-target="#ModalLogin">
-    تسجيل
-   </button>
-   <div dir='rtl' class="modal fade" id="ModalLogin"  tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="ModalLabel">مرحبا بك </h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-         لايوجد حاليا تسجيل في الموقع اذا كنت مهتم في التسجيل واصل التسجيل
-        </div>
-        <div class="modal-footer">
-          <button type="button" id="btnn" class="btn btn-danger"  data-bs-dismiss="modal">اغلاق</button>
-          <button type="button" class="btn btn-primary"  onClick={handleShow}>مواصلة التسجيل</button>
-          
+      <button type="button" class="btn btn-primary" className='sign-link sign-in' data-bs-toggle="modal" data-bs-target="#ModalLogin">
+        تسجيل
+      </button>
+      <div dir='rtl' class="modal fade" id="ModalLogin" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="ModalLabel">مرحبا بك </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              لايتوفر حالياً التسجيل في الموقع إذا كنت مهتم في أحدى النباتات ارسل لنا طلبك
+            </div>
+            <div class="modal-footer">
+              <div className="m-auto">
+                <button type="button" id="btnn" class="btn btn-danger mx-2" data-bs-dismiss="modal">اغلاق</button>
+                <button type="button" class="btn btn-success mx-2" onClick={handleShow}>أنا مهتم</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  <ProductInquiryForm
+      <ProductInquiryForm
         show={show}
         handleClose={handleClose}
         showAlert={showAlert}
         setShowAlert={setShowAlert}
         setStateMessage={setStateMessage}
-       
+
       />
     </>
-    
-   )
+
+  )
 }
 
 export default FormLogin;
 
 
-          
+
