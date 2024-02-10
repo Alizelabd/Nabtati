ad7835f77b8be465fa57bcf7383b031c3274c158
Alizelabd <lordlords834@gmail.com>
2024-02-10 21:20:16 +0300

    s

--- src/Pages/PlantsStories.js
+++ src/Pages/PlantsStories.js
@@ -56,64 +56,64 @@ function PlantsStories() {
 
   return (
     <div className="story-section">
-      
-        <Header />
-        <Container className="py-4">
-          <Container>
-            <Breadcrumb className="breadcrumbs">
-              <BreadcrumbLink
-                to="/"
-                label="الــرئـيـسـيـه"
-                className="non-link"
-              />
-              <BreadcrumbLink
-                to={`/products/${ProductId}`}
-                label={product.storyname}
-                active
-              />
-            </Breadcrumb>
-          </Container>
 
-          <div className="story-div">
-            <div className="info">
-              <div className="storylabel">
-                <h1>{product.storyname}</h1>
-              </div>
-              <div className="storyText">
-                <p>{product.story}</p>
-              </div>
-            </div>
-            <div className="image-plant">
-              <img
-                className="image-respon"
-                src={product.imageUrl2}
-                alt={product.name}
-              />
-            </div>
-          </div>
-          <div className="d-flex needs box-shadow my-4">
-            <div className="box2_text">
-              <text> احتياجات {product.name}</text>
-            </div>
-            <div className="plant_info">
-              <img src={plant_info} alt={product.name} />
-            </div>
-            <button
-              className="button_box2"
-              onClick={handleShow}
-            >{`أنا مهتم بـ ${product.name} `}</button>
-          </div>
+      <Header />
+      <Container className="py-4">
+        <Container>
+          <Breadcrumb className="breadcrumbs">
+            <BreadcrumbLink
+              to="/"
+              label="الــرئـيـسـيـه"
+              className="non-link"
+            />
+            <BreadcrumbLink
+              to={`/products/${ProductId}`}
+              label={product.storyname}
+              active
+            />
+          </Breadcrumb>
         </Container>
-        <Footer />
-        <ProductInquiryForm
-          show={show}
-          handleClose={handleClose}
-          showAlert={showAlert}
-          setShowAlert={setShowAlert}
-          setStateMessage={setStateMessage}
-          onInquirySubmit={handleInquirySubmit}
-        />
-      
+
+        <div className="story-div">
+          <div className="info">
+            <div className="storylabel">
+              <h1>{product.storyname}</h1>
+            </div>
+            <div className="storyText">
+              <p>{product.story}</p>
+            </div>
+          </div>
+          <div className="image-plant">
+            <img
+              className="image-respon"
+              src={product.imageUrl2}
+              alt={product.name}
+            />
+          </div>
+        </div>
+        <div className="d-flex needs box-shadow my-4">
+          <div className="box2_text">
+            <text> احتياجات {product.name}</text>
+          </div>
+          <div className="plant_info">
+            <img src={plant_info} alt={product.name} />
+          </div>
+          <button
+            className="button_box2"
+            onClick={handleShow}
+          >{`أنا مهتم بـ ${product.name} `}</button>
+        </div>
+      </Container>
+      <Footer />
+      <ProductInquiryForm
+        show={show}
+        handleClose={handleClose}
+        showAlert={showAlert}
+        setShowAlert={setShowAlert}
+        setStateMessage={setStateMessage}
+        onInquirySubmit={handleInquirySubmit}
+      />
+
     </div>
   );
 }
